1ec5f58225b558673b549303e2fadbf1f4d16ee0
monica nguyen-vo <monnvart@gmail.com>
2020-08-12 19:51:26 -0500

    backpack puzzle

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 using UnityEngine;
+using System.Linq;
 
 public class GameControl : MonoBehaviour
 {
@@ -23,22 +26,7 @@ public class GameControl : MonoBehaviour
     //Update is called once per frame
     void Update()
     {
-        if (pictures[1].rotation.z == 00 &&
-          pictures[2].rotation.z == 00 &&
-          pictures[3].rotation.z == 00 &&
-          pictures[4].rotation.z == 00 &&
-          pictures[5].rotation.z == 00 &&
-          pictures[6].rotation.z == 00 &&
-          pictures[7].rotation.z == 00 &&
-          pictures[8].rotation.z == 00 &&
-          pictures[9].rotation.z == 00 &&
-          pictures[10].rotation.z == 00 &&
-          pictures[11].rotation.z == 00 &&
-          pictures[12].rotation.z == 00 &&
-          pictures[13].rotation.z == 00 &&
-          pictures[14].rotation.z == 00 &&
-          pictures[15].rotation.z == 00 &&
-          pictures[16].rotation.z == 00)
+        if (pictures.All(picture => picture.rotation.z == 0))
         {
             youWin = true;
             winText.SetActive(true);
